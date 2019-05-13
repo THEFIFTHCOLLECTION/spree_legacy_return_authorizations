abf7823a9778d172e12ea9d8168f6f8502c1d77c
MittalPatel-BTC <mittal.patel@botreetechnologies.com>
2019-05-13 16:41:10 +0530

    Replace alias_method_chain method with prepend.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,10 +1,4 @@
 Spree::Order.class_eval do
   has_many :legacy_return_authorizations, dependent: :destroy
-
-  def awaiting_return_with_legacy_return_authorizations?
-    awaiting_return_without_legacy_return_authorizations? ||
-      legacy_return_authorizations.any? { |legacy_return_authorization| legacy_return_authorization.authorized? }
-  end
-
-  alias_method_chain :awaiting_return?, :legacy_return_authorizations
+  prepend Spree::AdminLegacyReturn
 end
